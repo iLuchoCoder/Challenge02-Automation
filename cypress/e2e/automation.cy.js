8bb2357e18c697ef5dcc5a5c79dac535b4dd86d7
Luis Pineda <lucho.pineda.88@gmail.com>
2024-10-14 19:31:05 -0500

    Commit userStory02 v2

--- cypress/e2e/automation.cy.js
+++ cypress/e2e/automation.cy.js
@@ -8,7 +8,7 @@ describe('Challenge02-Automation', () => {
     cy.contains('results for "Keyboards"')
   })
 
-  it.only('userStory02', () => {
+  it('userStory02', () => {
     cy.visit('https://www.amazon.com/')
     cy.get('#twotabsearchtextbox').type('Laptops{enter}')
     cy.get('div[class="s-widget-container s-spacing-small s-widget-container-height-small celwidget slot=MAIN template=SEARCH_RESULTS widgetId=search-results_1"] h2[class="a-size-mini a-spacing-none a-color-base s-line-clamp-2"] span:nth-child(1)').click()
